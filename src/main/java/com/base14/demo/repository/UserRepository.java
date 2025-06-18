d56632b0f966ad231125b567ca48bb7b52c12413
ShivaniBanke <shivani@infraspec.dev>
2025-06-18 13:58:12 +0530

    Add all files to spring boot folder

--- src/main/java/com/base14/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.base14.demo.repository;
-
-import com.base14.demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-    User findFirstById(Long id);
-}
